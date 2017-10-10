a5fe29dd4da55e10eb1aadc318f8b5b8db4f46ea
Romain Cazé <romain.caze@gmail.com>
2017-10-10 15:34:51 +0200

    Error in main

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from lib import run, group_size_ev, save_gsize
 from plot import pop, markov
 
-
 # Must do two separate runs because of Brian2
-"""
+repet_n = 1
 group_ev = []
 for i in range(1, 180, 5):
-    group_ev.append(group_size_ev(i, 50))
+    group_ev.append(group_size_ev(i, repet_n))
     print(i/180.)
 save_gsize(group_ev, linear=True)
-"""
 
 group_ev = []
 for i in range(1, 180, 5):
-    group_ev.append(group_size_ev(i, 50, linear=False))
+    group_ev.append(group_size_ev(i, repet_n, linear=False))
     print(i/180.)
 save_gsize(group_ev, linear=False)
